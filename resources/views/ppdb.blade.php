acab9fe50e833385da93cf76bc540db7206c67a0
lukimme <lukmanhakim8832@gmail.com>
2024-09-24 00:15:12 +0700

    ppdb & form

--- resources/views/ppdb.blade.php
+++ resources/views/ppdb.blade.php
@@ -2,139 +2,155 @@
 @section('title', 'PPDB SMK BM Pakisaji')
 @section('content')
 <section>
-  <!-- ======= Hero Section ======= -->
-  <section id="pdb" class="pdb">
-    <div class="container position-relative">
-        <div class="row gy-5" data-aos="fade-in">
-          <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center text-lg-start">
-            <h2>PPDB Online</h2>
-            <p>Untuk calon pendaftar tahun ajaran 2024/2025 bisa mendaftar melalui website ini atau langsung datang ke tempat pendaftaran</p>
-            <div class="d-flex justify-content-center justify-content-lg-start">
-              <a href="daftar" class="btn-get-started">Daftar Sekarang</a>
-              {{-- <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a> --}}
+    <!-- ======= Hero Section ======= -->
+    <section id="pdb" class="pdb">
+        <div class="container position-relative">
+            <div class="row gy-5" data-aos="fade-in">
+                <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center text-lg-start">
+                    <h2>PPDB Online</h2>
+                    <p>Untuk calon pendaftar tahun ajaran 2025/2027 bisa mendaftar melalui website ini atau langsung
+                        datang ke tempat pendaftaran</p>
+                    <div class="d-flex justify-content-center justify-content-lg-start">
+                        <a href="daftar" class="btn-get-started">Daftar Sekarang</a>
+                        {{-- <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a> --}}
+                    </div>
+                </div>
+                <div class="col-lg-6 order-1 order-lg-2">
+                    <img src="img/10089.png" class="img-fluid animated">
+                </div>
             </div>
-          </div>
-          <div class="col-lg-6 order-1 order-lg-2">
-            <img src="img/10089.png" class="img-fluid animated">
-          </div>
         </div>
-      </div>
-  </section>
+    </section>
 
-  <section id="about" class="about mt-0">
-    <div class="container aos-init aos-animate" data-aos="fade-up"> 
-        <div class="row align-items-center">
-            <div class="col-lg-5 order-1 order-lg-1 aos-init" data-aos="fade-up" data-aos-delay="100">
-                <img src="img/img4.png" class="img-fluid" alt="Gedung2">
+    <section id="about" class="about mt-0">
+        <div class="container aos-init aos-animate" data-aos="fade-up">
+            <div class="row align-items-center">
+                <div class="col-lg-5 order-1 order-lg-1 aos-init" data-aos="fade-up" data-aos-delay="100">
+                    <img src="img/img4.png" class="img-fluid" alt="Gedung2">
+                </div>
+                <div class="col-lg-7 pt-4 pt-lg-0 order-2 order-lg-2 content">
+                    <h3>Ada Apa di SMK Budi Mulia Pakisaji?</h3>
+                    <p>SMK Budi Mulia Pakisaji atau akrab dengan sebutan SMKBM, memiliki 5 Kompetensi Keahlian Unggulan
+                        & Milenial
+                        yang
+                        bisa dipilih sesuai
+                        dengan
+                        minat kalian, sini kepoin :</p>
+                    <ul>
+                        <li><i class="bi bi-check2-circle"></i> TKJ (Teknik Komputer & Jaringan)</li>
+                        <li><i class="bi bi-check2-circle"></i> DKV (Desain Komunikasi Visual)</li>
+                        <li><i class="bi bi-check2-circle"></i> BD (Bisnis Daring)</li>
+                        <li><i class="bi bi-check2-circle"></i> SIJA (Sistem Informasi Jaringan Aplikasi)</li>
+                        <li><i class="bi bi-check2-circle"></i> Otomotif </li>
+                    </ul>
+                    <br>
+                    <p><b>Eitss</b> bukan itu saja, Selain itu kita juga memiliki kelas yang hanya ada di SMK Budi
+                        Mulia.
+                        pasti kamu bertanya tanya "Kelas bagaimana tuh?" , yaudah ni kukasih tau ya namanya <b>Double
+                            Degree</b></p>
+
+                </div>
             </div>
-            <div class="col-lg-7 pt-4 pt-lg-0 order-2 order-lg-2 content">
-                <h3>Ada Apa di SMK Budi Mulia Pakisaji?</h3>
-                <p>SMK Budi Mulia Terdapat 4 Kompetensi Keahlian Unggulan & Milenial yang bisa dipilih sesuai dengan minat kalian, Diantaranya :</p>
-                <ul>
-                    <li><i class="bi bi-check2-circle"></i> TKJ (Teknik Komputer & Jaringan)</li>
-                    <li><i class="bi bi-check2-circle"></i> DKV (Desain Komunikaasi Visual)</li>
-                    <li><i class="bi bi-check2-circle"></i> BD (Bisnis Daring)</li>
-                    <li><i class="bi bi-check2-circle"></i> SIJA (Sistem Informasi Jaringan Aplikasi)</li>
-                </ul>
-                <br>
-                <p><b>Eitss</b> bukan itu saja, Selain itu kita juga memiliki kelas yang hanya ada di SMK Budi Mulia. 
-                pasti kamu bertanya tanya "Kelas bagaimana tuh?" , yaudah ni kukasih tau ya namanya <b>Double Degree</b></p>
+        </div>
+
+        <div class="container aos-init aos-animate mt-5" data-aos="fade-up">
+            <div class="row align-items-center">
+                <div class="col-lg-5 order-2 order-lg-2 aos-init" data-aos="fade-up" data-aos-delay="100">
+                    <img src="img/img6.png" class="img-fluid" alt="Gedung2">
+                </div>
+                <div class="col-lg-7 pt-4 pt-lg-0 order-1 order-lg-1 content">
+                    <h3>Apa Kelebihan dari Doubel Degree itu?</h3>
+                    <p><b>Double Degree</b> adalah kelas yang dimana Selain Mendapat Ijazah siswa - siswi yang tergabung
+                        dalam kelas ini akan mendapat sertifikat kompetensi dari industri rekanan</p>
+                    <p>kelas <b>Double Degree</b> antara lain :</p>
+                    <ul>
+                        <li><i class="bi bi-check2-circle"></i> Perhotelan</li>
+                        <li><i class="bi bi-check2-circle"></i> Tata Boga</li>
+                        <li><i class="bi bi-check2-circle"></i> Otomotif</li>
+                        <li><i class="bi bi-check2-circle"></i> MUA</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="container aos-init aos-animate mt-5" data-aos="fade-up">
+            <div class="row align-items-center">
+                <div class="col-lg-5 pt-4 pt-lg-0 order-1 order-lg-2 content">
+                    <p>Dengan Bergabung di SMK budi Mulia Pakisaji Kalian tidak Menunggu <b>LULUS</b> Baru bisa Dapat
+                        <b>PENGHASILAN</b> Tapi Saat <b>SEKOLAH</b> Kalian Sudah Bisa Dapat <b>Gaji</b> & dapat
+                        <b>Bersekolah Gratis</b>.
+                    </p>
+                    {{-- <p><b>Free</b> Formulir Untuk Pendaftaran di Bulan <b>Desember</b></p> --}}
+                    {{-- <p>Kalian Tertarik gak nih?? Segera Daftarkan Diri Kalian Disini dan dapatkan bonus menarik lainnya <b>bagi 100 pendaftar pertama</b>. untuk daftar bisa klik link di bawah ini</p> --}}
+                </div>
+                <div class="col-lg-7 pt-4 pt-lg-0 order-2 order-lg-1">
+                    <img src="img/img7.png" class="img-fluid" alt="Gedung2">
+                </div>
 
             </div>
         </div>
-    </div>
 
-    <div class="container aos-init aos-animate mt-5" data-aos="fade-up"> 
-        <div class="row align-items-center">
-            <div class="col-lg-5 order-2 order-lg-2 aos-init" data-aos="fade-up" data-aos-delay="100">
-                <img src="img/img6.png" class="img-fluid" alt="Gedung2">
-            </div>
-            <div class="col-lg-7 pt-4 pt-lg-0 order-1 order-lg-1 content">
-                <h3>Apa Kelebihan dari Doubel Degree itu?</h3>
-                <p><b>Double Degree</b> adalah kelas yang dimana Selain Mendapat Ijazah siswa - siswi yang tergabung  dalam kelas ini akan mendapat sertifikat kompetensi dari industri rekanan</p>
-                <p>kelas <b>Double Degree</b> antara lain :</p>
-                <ul>
-                    <li><i class="bi bi-check2-circle"></i> Perhotelan</li>
-                    <li><i class="bi bi-check2-circle"></i> Otomotif</li>
-                    <li><i class="bi bi-check2-circle"></i> MIIA & Barber</li>
-                </ul>
+        <div class="container aos-init aos-animate mt-3" data-aos="fade-up">
+            <div class="row align-items-center">
+                <div class="col-lg-4 pt-4 pt-lg-0 order-2 order-lg-2 d-none d-lg-block">
+                    <img src="img/img2222.png" class="img-fluid" alt="Gedung2">
+                </div>
+                <div class="col-lg-8 pt-4 pt-lg-0 order-1 order-lg-1 content">
+                    <!--<p><b>Free</b> Formulir Untuk Pendaftaran di Bulan <b>Desember</b></p>-->
+                    <p>jadi gimana Kalian Tertarik gak nih?? Segera Daftarkan Diri Kalian Disini dan dapatkan bonus
+                        menarik lainnya <b>bagi 100 pendaftar pertama</b>. untuk daftar bisa klik link di bawah ini</p>
+                </div>
             </div>
         </div>
-    </div>
 
-    <div class="container aos-init aos-animate mt-5" data-aos="fade-up"> 
-        <div class="row align-items-center">
-            <div class="col-lg-5 pt-4 pt-lg-0 order-1 order-lg-2 content">
-            <p>Dengan Bergabung di SMK budi Mulia Pakisaji Kalian tidak Menunggu <b>LULUS</b> Baru bisa Dapat <b>PENGHASILAN</b> Tapi Saat <b>SEKOLAH</b> Kalian Sudah Bisa Dapat <b>Gaji</b> & dapat <b>Bersekolah Gratis</b>.</p>
-            {{-- <p><b>Free</b> Formulir Untuk Pendaftaran di Bulan <b>Desember</b></p> --}}
-            {{-- <p>Kalian Tertarik gak nih?? Segera Daftarkan Diri Kalian Disini dan dapatkan bonus menarik lainnya <b>bagi 100 pendaftar pertama</b>. untuk daftar bisa klik link di bawah ini</p> --}}
-            </div>
-            <div class="col-lg-7 pt-4 pt-lg-0 order-2 order-lg-1">
-                <img src="img/img7.png" class="img-fluid" alt="Gedung2">
-            </div>
-              
-        </div>
-    </div>
-    
-    <div class="container aos-init aos-animate mt-3" data-aos="fade-up"> 
-        <div class="row align-items-center">
-            <div class="col-lg-4 pt-4 pt-lg-0 order-2 order-lg-2 d-none d-lg-block">
-                <img src="img/img2222.png" class="img-fluid" alt="Gedung2">
-            </div>
-            <div class="col-lg-8 pt-4 pt-lg-0 order-1 order-lg-1 content">
-                <!--<p><b>Free</b> Formulir Untuk Pendaftaran di Bulan <b>Desember</b></p>-->
-                <p>jadi gimana Kalian Tertarik gak nih?? Segera Daftarkan Diri Kalian Disini dan dapatkan bonus menarik lainnya <b>bagi 100 pendaftar pertama</b>. untuk daftar bisa klik link di bawah ini</p>
+        <div class="container aos-init aos-animate mt-3" data-aos="fade-up" id="gel">
+            <div class="row align-items-center">
+                <div class="col-12 pt-4 pt-lg-0 text-center">
+                    <!--<img src="img/pdb.jpg"class="img-fluid" style="width:70%;height:auto;">-->
+                    <!--<a href="img/pdb.jpg" class="fullscreen img-fluid"><img src="img/pdb.jpg"  style="width:70%;height:auto;"></a>-->
+                </div>
             </div>
         </div>
-    </div>
-    
-    <div class="container aos-init aos-animate mt-3" data-aos="fade-up" id="gel"> 
-        <div class="row align-items-center">
-            <div class="col-12 pt-4 pt-lg-0 text-center">
-                <!--<img src="img/pdb.jpg"class="img-fluid" style="width:70%;height:auto;">-->
-                <!--<a href="img/pdb.jpg" class="fullscreen img-fluid"><img src="img/pdb.jpg"  style="width:70%;height:auto;"></a>-->
+
+    </section>
+    <section id="" class="container text-center my-0">
+        <div class="row justify-content-around">
+            <div class="col-6">
+                <a href="daftar" class="btn-get-started text-white" style="font-size:13px;">Daftar Sekarang</a>
+            </div>
+            <div class="col-6">
+                <!--<a href="https://wa.me/+6289513009008 "></a>-->
+                <a href="https://api.whatsapp.com/send?phone=6289513009008&text=Hai%20admin,%0Aboleh%20tanya%20tentang%20*PPDB%20SMK%20Budi%20Mulia%20Pakisaji*"
+                    class="btn-get-started text-center text-white" style="font-size:13px;">
+                    <i class="bi bi-whatsapp"></i> Hubungi Kami
+                </a>
             </div>
         </div>
-    </div>
-    
-  </section>
-  <section id="" class="container text-center my-0">
-    <div class="row justify-content-around">
-        <div class="col-6">
-            <a href="daftar" class="btn-get-started text-white" style="font-size:13px;">Daftar Sekarang</a>
-        </div>
-        <div class="col-6">
-            <!--<a href="https://wa.me/+6289513009008 "></a>-->
-            <a href="https://api.whatsapp.com/send?phone=6289513009008&text=Hai%20admin,%0Aboleh%20tanya%20tentang%20*PPDB%20SMK%20Budi%20Mulia%20Pakisaji*" class="btn-get-started text-center text-white"  style="font-size:13px;">
-                <i class="bi bi-whatsapp"></i> Hubungi Kami
-            </a>
-        </div>
-    </div>
-  </section>
+    </section>
 </section>
 <!---->
-   <style>
-      .fullscreen img {
-         max-width: 100%;
-         max-height: 100%;
-         display: block;
-         margin: auto;
-      }
-   </style>
-   <script>
-   document.addEventListener('click', function(e) {
-      if (e.target.classList.contains('fullscreen')) {
-         e.preventDefault();
-         var imgSrc = e.target.tagName === 'IMG' ? e.target.src : e.target.href;
-         var overlay = document.createElement('div');
-         overlay.classList.add('fullscreen');
-         overlay.innerHTML = '<img src="' + imgSrc + '">';
-         document.body.appendChild(overlay);
-         overlay.addEventListener('click', function() {
+<style>
+.fullscreen img {
+    max-width: 100%;
+    max-height: 100%;
+    display: block;
+    margin: auto;
+}
+</style>
+<script>
+document.addEventListener('click', function(e) {
+    if (e.target.classList.contains('fullscreen')) {
+        e.preventDefault();
+        var imgSrc = e.target.tagName === 'IMG' ? e.target.src : e.target.href;
+        var overlay = document.createElement('div');
+        overlay.classList.add('fullscreen');
+        overlay.innerHTML = '<img src="' + imgSrc + '">';
+        document.body.appendChild(overlay);
+        overlay.addEventListener('click', function() {
             overlay.parentNode.removeChild(overlay);
-         });
-      }
-   });
+        });
+    }
+});
 </script>
 
 <!---->
