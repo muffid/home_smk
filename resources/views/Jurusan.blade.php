2600b2840c8573225958d50d543e6862cae83788
lukimme <lukmanhakim8832@gmail.com>
2024-10-03 01:26:49 +0700

    test

--- resources/views/Jurusan.blade.php
+++ resources/views/Jurusan.blade.php
@@ -5,65 +5,92 @@
 @section('content')
 
 <div class="col-xs-12 text-light">
-  <div class="row align-items-center">
-    <div class="col-lg-6 mb-3">
-      <span class="text-light mb-5"></span>
-      <h2 class="fw-bold">Menjadi Ahli Teknologi Informasi dan Komunikasi (TIK) dengan Program SIJA di SMK Budi Mulia Pakisaji</h2 class="fw-bold mb-3">
-      
-      <div class="d-flex align-items-center">
-        <h6 class="d-inline ms-3">admin</h6>
-      </div>
-    </div>
+    <div class="row align-items-center">
+        <div class="col-lg-6 mb-3">
+            <span class="text-light mb-5"></span>
+            <h2 class="fw-bold">Menjadi Ahli Teknologi Informasi dan Komunikasi (TIK) dengan Program SIJA di SMK Budi
+                Mulia Pakisaji</h2 class="fw-bold mb-3">
 
-    <div class="col-lg-6">
-      <img src="https://img.freepik.com/free-photo/high-angle-nutritional-counter-app-composition_23-2149880622.jpg?size=626&ext=jpg&ga=GA1.2.1853520817.1676958642&semt=ais" alt="" class="img-fluid rounded">
+            <div class="d-flex align-items-center">
+                <h6 class="d-inline ms-3">admin</h6>
+            </div>
+        </div>
+
+        <div class="col-lg-6">
+            <img src="https://img.freepik.com/free-photo/high-angle-nutritional-counter-app-composition_23-2149880622.jpg?size=626&ext=jpg&ga=GA1.2.1853520817.1676958642&semt=ais"
+                alt="" class="img-fluid rounded">
+        </div>
     </div>
-  </div>
 </div>
 
 <div class="col-xs-12 text-light mt-3">
-  <div class="row">
-      <div class="col-lg-8 col-xs-12">
-        <div class="card border-0 bg-transparent">
-          <div class="card-body text-start content" id="content">
-                <p>Jurusan Sistem Informasi Jaringan & Aplikasi (SIJA) di SMK Budi Mulia Pakisaji adalah program keahlian yang bertujuan untuk menghasilkan tenaga ahli di bidang teknologi informasi dan komunikasi (TIK) khususnya pada bidang pengembangan jaringan dan aplikasi. Program SIJA dirancang untuk mengajarkan siswa tentang berbagai teknologi dan aplikasi dalam bidang TIK, serta mengembangkan kemampuan mereka dalam merancang dan mengembangkan aplikasi dan jaringan yang efektif dan efisien.</p>
-                <p>Program SIJA di SMK Budi Mulia Pakisaji terdiri dari dua fokus utama, yaitu sistem informasi jaringan dan pengembangan aplikasi. Fokus pada sistem informasi jaringan mencakup pelajaran tentang perancangan, instalasi, konfigurasi, dan pemeliharaan jaringan komputer. Siswa akan belajar tentang topologi jaringan, protokol, dan metode keamanan jaringan. Selain itu, siswa juga akan mempelajari cara memperbaiki dan mengatasi masalah dalam jaringan komputer.</p>
-                <p>Fokus pada pengembangan aplikasi mencakup pelajaran tentang perancangan dan pengembangan aplikasi berbasis web dan aplikasi desktop. Siswa akan diajarkan tentang pemrograman berbagai bahasa pemrograman seperti PHP, Java, HTML, CSS, dan JavaScript. Selain itu, siswa juga akan mempelajari cara membangun aplikasi dengan database dan mengimplementasikan sistem keamanan dalam aplikasi.</p>
-                <p>Melalui program SIJA di SMK Budi Mulia Pakisaji, siswa akan dilatih menggunakan berbagai perangkat dan teknologi terkini dalam bidang TIK. Siswa akan belajar menggunakan perangkat lunak seperti MySQL, XAMPP, Visual Studio, dan Adobe Dreamweaver, serta menggunakan perangkat keras seperti komputer desktop, laptop, dan perangkat jaringan.</p>
-                <p>Di samping itu, program SIJA juga mengajarkan keterampilan yang diperlukan untuk bekerja dalam tim, mengembangkan kemampuan komunikasi yang baik, dan menumbuhkan sikap yang profesional dalam bekerja. Siswa akan diajarkan untuk berpikir kritis dan kreatif dalam menyelesaikan masalah, serta mengembangkan kecakapan yang diperlukan untuk bekerja dalam industri TIK.</p>
-                <p>Setelah menyelesaikan program SIJA di SMK Budi Mulia Pakisaji, siswa akan memiliki peluang karir yang luas di bidang TIK. Mereka dapat bekerja sebagai administrator jaringan, pengembang aplikasi web atau desktop, dan spesialis keamanan informasi. Selain itu, siswa juga dapat melanjutkan pendidikan mereka di perguruan tinggi dan memperoleh gelar sarjana di bidang teknologi informasi dan komunikasi.</p>
-                <p>Secara keseluruhan, program SIJA di SMK Budi Mulia Pakisaji adalah pilihan yang tepat bagi siswa yang tertarik dengan pengembangan jaringan dan aplikasi. Melalui program ini, siswa akan dilatih untuk menjadi ahli dalam bidang TIK dan siap untuk menghadapi tantangan di masa depan.</p>
+    <div class="row">
+        <div class="col-lg-8 col-xs-12">
+            <div class="card border-0 bg-transparent">
+                <div class="card-body text-start content" id="content">
+                    <p>Jurusan Sistem Informasi Jaringan & Aplikasi (SIJA) di SMK Budi Mulia Pakisaji adalah program
+                        keahlian yang bertujuan untuk menghasilkan tenaga ahli di bidang teknologi informasi dan
+                        komunikasi (TIK) khususnya pada bidang pengembangan jaringan dan aplikasi. Program SIJA
+                        dirancang untuk mengajarkan siswa tentang berbagai teknologi dan aplikasi dalam bidang TIK,
+                        serta mengembangkan kemampuan mereka dalam merancang dan mengembangkan aplikasi dan jaringan
+                        yang efektif dan efisien.</p>
+                    <p>Program SIJA di SMK Budi Mulia Pakisaji terdiri dari dua fokus utama, yaitu sistem informasi
+                        jaringan dan pengembangan aplikasi. Fokus pada sistem informasi jaringan mencakup pelajaran
+                        tentang perancangan, instalasi, konfigurasi, dan pemeliharaan jaringan komputer. Siswa akan
+                        belajar tentang topologi jaringan, protokol, dan metode keamanan jaringan. Selain itu, siswa
+                        juga akan mempelajari cara memperbaiki dan mengatasi masalah dalam jaringan komputer.</p>
+                    <p>Fokus pada pengembangan aplikasi mencakup pelajaran tentang perancangan dan pengembangan aplikasi
+                        berbasis web dan aplikasi desktop. Siswa akan diajarkan tentang pemrograman berbagai bahasa
+                        pemrograman seperti PHP, Java, HTML, CSS, dan JavaScript. Selain itu, siswa juga akan
+                        mempelajari cara membangun aplikasi dengan database dan mengimplementasikan sistem keamanan
+                        dalam aplikasi.</p>
+                    <p>Melalui program SIJA di SMK Budi Mulia Pakisaji, siswa akan dilatih menggunakan berbagai
+                        perangkat dan teknologi terkini dalam bidang TIK. Siswa akan belajar menggunakan perangkat lunak
+                        seperti MySQL, XAMPP, Visual Studio, dan Adobe Dreamweaver, serta menggunakan perangkat keras
+                        seperti komputer desktop, laptop, dan perangkat jaringan.</p>
+                    <p>Di samping itu, program SIJA juga mengajarkan keterampilan yang diperlukan untuk bekerja dalam
+                        tim, mengembangkan kemampuan komunikasi yang baik, dan menumbuhkan sikap yang profesional dalam
+                        bekerja. Siswa akan diajarkan untuk berpikir kritis dan kreatif dalam menyelesaikan masalah,
+                        serta mengembangkan kecakapan yang diperlukan untuk bekerja dalam industri TIK.</p>
+                    <p>Setelah menyelesaikan program SIJA di SMK Budi Mulia Pakisaji, siswa akan memiliki peluang karir
+                        yang luas di bidang TIK. Mereka dapat bekerja sebagai administrator jaringan, pengembang
+                        aplikasi web atau desktop, dan spesialis keamanan informasi. Selain itu, siswa juga dapat
+                        melanjutkan pendidikan mereka di perguruan tinggi dan memperoleh gelar sarjana di bidang
+                        teknologi informasi dan komunikasi.</p>
+                    <p>Secara keseluruhan, program SIJA di SMK Budi Mulia Pakisaji adalah pilihan yang tepat bagi siswa
+                        yang tertarik dengan pengembangan jaringan dan aplikasi. Melalui program ini, siswa akan dilatih
+                        untuk menjadi ahli dalam bidang TIK dan siap untuk menghadapi tantangan di masa depan.</p>
+                </div>
+
+                <style>
+                .content blockquote {
+                    background-color: none;
+                    border-left: 10px solid #1b84a3;
+                    margin: 20px 0;
+                    padding: 20px;
+                    font-style: italic;
+                }
+
+                .content img {
+                    float: left;
+                    /* margin-right: 20px; */
+                    width: 100%;
+                    height: auto;
+                    margin: 10px 20px 10px 0;
+                }
+                </style>
             </div>
-
-          <style>
-            .content blockquote {
-              background-color: none;
-              border-left: 10px solid #1b84a3;
-              margin: 20px 0;
-              padding: 20px;
-              font-style: italic;
-            }
-            .content img {
-              float: left;
-              /* margin-right: 20px; */
-              width: 100%;
-              height: auto;
-              margin: 10px 20px 10px 0;
-            }
-
-          </style>
         </div>
-      </div>
-      <div class="col-lg-4 col-xs-12 d-md-2 d-none">
-        <hr>
-        <div class="sticky-top" style="height:100vh;">
-          <h5>Berita Terpopuler</h5>
-          <ul style="list-style:none;">
-          
-          </ul>
+        <div class="col-lg-4 col-xs-12 d-md-2 d-none">
+            <hr>
+            <div class="sticky-top" style="height:100vh;">
+                <h5>Berita Terpopuler</h5>
+                <ul style="list-style:none;">
+
+                </ul>
+            </div>
         </div>
-      </div>
-  </div>
+    </div>
 
 
-@endsection
+    @endsection
